18626b16036835223963fadd81b42d55a15a3a78
anassohail99 <anas.sohailabs1@gmail.com>
2020-06-12 03:51:50 +0500

    Final version of App JS file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+
+const api = {
+  key: "90d51bc5e956584052252501ee0024d1",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
-  return <div className="App"></div>;
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery("");
+          setWeather(result);
+          console.log(result);
+        });
+    }
+  };
+
+  let date = String(new window.Date());
+  date = date.slice(0, 15);
+
+  return (
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 20
+            ? "app warm"
+            : "app"
+          : "app"
+      }
+    >
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        <div>
+          {typeof weather.main != "undefined" ? (
+            <div className="location-box">
+              <div className="location">
+                {weather.name} , {weather.sys.country}
+              </div>
+              <div className="date">{date}</div>
+              <div className="weather-box">
+                <div className="temp">{Math.round(weather.main.temp)}°c</div>
+                <div className="weather">{weather.weather[0].main}</div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
